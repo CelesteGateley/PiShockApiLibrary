15a7dd65f17439d2ee982f7ee2f7a111d368b9e8
Celeste Gateley <celeste.gateley@gmail.com>
2026-09-16 13:23:51 +0100

    Implemented better handling for v3 api

---
+++ PishockExceptions.cs
@@ -0,0 +1,36 @@
+﻿namespace PiShockApiLibrary;
+
+public class PishockException: Exception
+{
+    public PishockException() { }
+    public PishockException(string message) : base(message) { }
+    public PishockException(string message, Exception innerException) : base(message, innerException) { }
+}
+
+public class PishockAuthenticationException : PishockException
+{
+    public PishockAuthenticationException() { }
+    public PishockAuthenticationException(string message) : base(message) { }
+    public PishockAuthenticationException(string message, Exception innerException) : base(message, innerException) { }
+}
+
+public class PishockPermissionException : PishockException
+{
+    public PishockPermissionException() { }
+    public PishockPermissionException(string message) : base(message) { }
+    public PishockPermissionException(string message, Exception innerException) : base(message, innerException) { }
+}
+
+public class PishockDataException : PishockException
+{
+    public PishockDataException() { }
+    public PishockDataException(string message) : base(message) { }
+    public PishockDataException(string message, Exception innerException) : base(message, innerException) { }
+}
+
+public class PishockShockerException : PishockException
+{
+    public PishockShockerException() { }
+    public PishockShockerException(string message) : base(message) { }
+    public PishockShockerException(string message, Exception innerException) : base(message, innerException) { }
+}
--- Shocker.cs
+++ Shocker.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http.Json;
+﻿using System.Net;
+using System.Net.Http.Json;
 using System.Text.Json.Serialization;
 
 namespace PiShockApiLibrary;
@@ -9,30 +10,71 @@ public class Shocker(string apiKey, string shockerId, string agent = "C# PiShock
 
     public async Task Shock(float duration, int intensity)
     {
-        await Activate(0, duration, intensity);
+        await ActivateV3(0, duration, intensity);
     }
 
     public async Task Vibrate(float duration, int intensity)
     {
-        await Activate(1, duration, intensity);
+        await ActivateV3(1, duration, intensity);
     }
 
     public async Task Beep(float duration)
     {
-        await Activate(2, duration, 0);
+        await ActivateV3(2, duration, 0);
     }
 
-    private async Task Activate(int mode, double duration, int intensity)
+    private async Task ActivateV3(int mode, double duration, int intensity)
     {
         if (mode is < 0 or > 2) { throw new ArgumentOutOfRangeException(nameof(mode), mode, "Mode must be either 0 (Shock), 1 (Vibrate) or 2 (Beep)"); }
-        if (duration is < 0.3 or > 10) { throw new ArgumentOutOfRangeException(nameof(duration), duration, "Duration must be between 0.3 seconds and 10 seconds."); }
+        if (duration is < 0.3 or > 15) { throw new ArgumentOutOfRangeException(nameof(duration), duration, "Duration must be between 0.3 seconds and 15 seconds."); }
         if (intensity is < 0 or > 100) { throw new ArgumentOutOfRangeException(nameof(intensity), intensity, "Intensity must be between 0 and 100."); }
         
         var request = new HttpRequestMessage(HttpMethod.Post, "https://api.pishock.com/Shockers/" + shockerId);
         request.Content = JsonContent.Create(new ShockerRequestData(mode, duration, intensity, agent));
         request.Headers.Add("X-Pishock-Api-Key", apiKey);
         
-        var response = await Client.SendAsync(request);
+        ValidateV3Response(await Client.SendAsync(request));
+    }
+
+    private void ValidateV3Response(HttpResponseMessage response)
+    {
+        Console.WriteLine(response);
+        if (response.IsSuccessStatusCode) return;
+
+        // ReSharper disable once SwitchStatementHandlesSomeKnownEnumValuesWithDefault
+        switch (response.StatusCode)
+        {
+            // Bad API Key errors
+            case HttpStatusCode.Unauthorized:
+                throw new PishockAuthenticationException($"Invalid/missing API key. ({response.Content})");
+            case HttpStatusCode.Forbidden:
+                throw new PishockAuthenticationException($"API Key does not have permission to perform action. ({response.Content})");
+                
+            // PiShock Permissions Error (Shocker Not found/available etc)
+            case HttpStatusCode.MethodNotAllowed:
+                throw new PishockPermissionException($"Cannot use requested mode with this shocker. ({response.Content})");
+            case HttpStatusCode.NotFound:
+                throw new PishockPermissionException($"Requested shocker could not be found. ({response.Content})");
+            case HttpStatusCode.Gone:
+                throw new PishockPermissionException($"Requested share is locked. ({response.Content})");
+            case HttpStatusCode.ServiceUnavailable:
+                throw new PishockPermissionException($"Requested shocker {shockerId} is currently paused. ({response.Content})");
+                
+            // Bad data passed to API
+            case HttpStatusCode.PreconditionFailed:
+                throw new PishockDataException($"Intensity was outside acceptable range. ({response.Content})");
+            case HttpStatusCode.RequestedRangeNotSatisfiable:
+                throw new PishockDataException($"Duration was outside acceptable range. ({response.Content})");
+                
+            // PiShock does not support V3
+            case HttpStatusCode.NotAcceptable:
+                throw new PishockShockerException("Shocker " + shockerId + " does not support V3 API.");
+                
+            // Fallback
+            case HttpStatusCode.InternalServerError:
+            default:
+                throw new PishockException($"An error occured with the PiShock API. ({response.Content})");
+        }
     }
 
     private class ShockerRequestData(int mode, double duration, int intensity, string agent)

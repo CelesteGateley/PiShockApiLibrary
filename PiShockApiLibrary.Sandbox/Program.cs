0f0d64f858f2afc0124529aeaaecc5196bd22480
Celeste Gateley <celeste.gateley@gmail.com>
2026-09-16 20:56:01 +0100

    Updated sandbox to test Legacy shocker

--- PiShockApiLibrary.Sandbox/Program.cs
+++ PiShockApiLibrary.Sandbox/Program.cs
@@ -4,19 +4,21 @@ public static class Program
 {
     public static async Task Main(string[] args)
     {
-        if (args.Length < 2)
+        if (args.Length < 3)
         {
-            Console.WriteLine("Usage: {api_key} {shocker_id}");
+            Console.WriteLine("Usage: {api_key} {shocker_id} {username} [share_code]");
             return;
         }
         var apiKey = args[0];
         var shockerId = args[1];
-        
-        Console.WriteLine("Api Key: " + apiKey);
-        Console.WriteLine("ShockerId: " + shockerId);
-        
-        var shocker = await ShockerFactory.CreateShocker(apiKey, shockerId);
-        await shocker.Vibrate(2, 5, 1, 1);
-        await shocker.Beep(2, 1);
+        var username = args[2];
+        string? shareCode = null;
+        if (args.Length > 3)
+        {
+            shareCode = args[3];
+        }
+
+        var shocker = await ShockerFactory.CreateLegacyShocker(apiKey, username, shockerId, shareCode);
+        await shocker.Beep(1);
     }
 }
